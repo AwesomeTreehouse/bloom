18d40f7cfd268a9fafe23110d8c722dec877b626
Robert Paniconi <robertpaniconi@gmail.com>
2017-10-31 14:32:30 -0400

    Updated Router

--- app/javascript/react/src/App.js
+++ app/javascript/react/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Router, browserHistory, Route, IndexRoute } from 'react-router';
+import { Router, browserHistory, Route } from 'react-router';
 import IndexContainer from './containers/IndexContainer'
 
 const App = props => {
   return(
     <Router history={browserHistory}>
-      <Route path='/'>
-        <IndexRoute component={IndexContainer} />
+      <Route path='/' component={IndexContainer}>
       </Route>
     </Router>
   )
