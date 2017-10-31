21045deb322949876e64103ec8ac91eb5052db07
Robert Paniconi <robertpaniconi@gmail.com>
2017-10-31 14:17:45 -0400

    Got rid of Devise. Added Google OmniAuth with appropriate testing. Replaced React NavBar with Rails top-bar

---
+++ db/migrate/20171031161939_remove_columns_from_users.rb
@@ -0,0 +1,16 @@
+class RemoveColumnsFromUsers < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :users, :encrypted_password
+    remove_column :users, :reset_password_token
+    remove_column :users, :reset_password_sent_at
+    remove_column :users, :remember_created_at
+    remove_column :users, :sign_in_count
+    remove_column :users, :current_sign_in_at
+    remove_column :users, :last_sign_in_at
+    remove_column :users, :current_sign_in_ip
+    remove_column :users, :last_sign_in_ip
+    remove_column :users, :role
+    remove_column :users, :username
+    remove_column :users, :avatar_url
+  end
+end
---
+++ db/migrate/20171031162948_add_columns_to_users.rb
@@ -0,0 +1,12 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :uid, :string
+    add_column :users, :provider, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :token, :string
+    add_column :users, :refresh_token, :string
+    add_column :users, :avatar_url, :string
+    add_column :users, :oauth_expires_at, :datetime
+  end
+end
