d5ac76d6448d59fa5f7f570772c1ee2017a1042d
Robert Paniconi <robertpaniconi@gmail.com>
2017-11-08 12:49:12 -0500

    Coffee Roaster search component

--- app/javascript/react/src/containers/CoffeeFinder.js
+++ app/javascript/react/src/containers/CoffeeFinder.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import InputField from '../components/InputField'
 
 class CoffeeFinder extends Component {
   constructor(props) {
@@ -12,8 +11,21 @@ class CoffeeFinder extends Component {
 
   render() {
     return(
-      <div>
-        Enter your zip-code to find the nearest coffee roaster<InputField/>
+      <div className="coffee-search-showpage">
+        <div className="text-center">
+          <form>
+            <fieldset className="fieldset">
+              <legend id="find-coffee">FIND MORE COFFEE</legend>
+
+              <label>
+                <input id='name' name='name' type='number' pattern="[0-9.]*" placeholder="Enter your zip-code"/>
+              </label>
+              <button className="button custom" href="#" >
+                SEARCH
+              </button>
+            </fieldset>
+          </form>
+        </div>
       </div>
     );
   }
