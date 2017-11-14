d180b4e2c1e58411426888d1dc2809a0f0c43211
Robert Paniconi <robertpaniconi@gmail.com>
2017-11-14 18:14:47 -0500

    Made changes to Formula update

--- app/controllers/coffee_formulas_controller.rb
+++ app/controllers/coffee_formulas_controller.rb
@@ -2,29 +2,6 @@ class CoffeeFormulasController < ApplicationController
   before_action :authorize_user, except: [:index]
 
   def index
-    # @coffee_formulas = CoffeeFormula.all
-    # if current_user
-    #@user = current_user
-    # else
-    #   @user = nil
-    # end
   end
-
-  def show
-    # coffee_formula = CoffeeFormula.find(params[:id])
-    # if current_user
-    #   @user = current_user.id
-    # else
-    #   @user = nil
-    # end
-  end
-
-  protected
-
-  # def authorize_user
-  #   if !user_signed_in? || !current_user.admin?
-  #     raise ActionController::RoutingError.new("Not Found")
-  #   end
-  # end
 
 end
